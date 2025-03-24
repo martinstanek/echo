67e3c253232c1370712f350e41176b7f8a2a599a
Martin Stanek <stanekmartin@bluewin.ch>
2025-03-24 17:10:20 +0100

    redirect

--- src/Echo.Host/Extensions/WebApplicationExtensions.cs
+++ src/Echo.Host/Extensions/WebApplicationExtensions.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Mvc;
 using Echo.Host.Handlers;
@@ -13,6 +14,18 @@ internal static class WebApplicationExtensions
         api.MapGet("/", ([FromServices] EchoHandler handler) => handler.GetIndex());
         api.MapGet("echo", ([FromServices] EchoHandler handler) => handler.GetEcho(string.Empty));
         api.MapGet("echo/{message}", ([FromRoute] string message, [FromServices] EchoHandler handler) => handler.GetEcho(message));
+
+        return webApplication;
+    }
+
+    public static WebApplication UseHttpsRedirect(this WebApplication webApplication)
+    {
+        var variable = Environment.GetEnvironmentVariable("ECHO_HTTPSREDIRECT");
+
+        if (bool.TryParse(variable, out var useRedirect) && useRedirect)
+        {
+            webApplication.UseHttpsRedirection();
+        }
 
         return webApplication;
     }
